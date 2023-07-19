4750f1bfe975ac688701c3a821facc19dcceaff1
Yac19 <sklatch99@proton.me>
2023-07-19 15:03:40 +0200

    Init java project for jenkins - exercise

---
+++ Javajenkins.java
@@ -0,0 +1,10 @@
+public class Javajenkins {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, that's my first java script !");
+        Additionner(1, 2);
+    }
+
+    public static int Additionner(int a, int b) {
+        return (a + b);
+    }
+}
